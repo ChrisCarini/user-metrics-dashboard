3062556823ca6c90780176401482743e6412fd55
ChrisCarini <6374067+ChrisCarini@users.noreply.github.com>
2024-09-16 04:55:48 -0700

    Update index.tsx - full screen width

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -3,7 +3,7 @@ import RepositoriesTable from '@/components/RepositoriesTable';
 
 export default function HomePage() {
   return (
-    <main className="p-4 md:p-10 mx-auto max-w-7xl h-screen">
+    <main className="p-4 md:p-10 mx-auto h-screen">
       <Layout>
         <RepositoriesTable />
       </Layout>
